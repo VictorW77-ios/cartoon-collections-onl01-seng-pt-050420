4a164c1ba9277ac734b6bf7f6f05ef1f6b91452a
Victor Williams <victor.williams1030@gmail.com>
2020-05-13 18:58:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(calls_array)
       false
     end
   end
-  if new_array 
+  if new_array.any?(true)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
