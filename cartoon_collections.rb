3ddf8c460bd42e4ebbb4a81caed4941cdfbd2a2a
Victor Williams <victor.williams1030@gmail.com>
2020-05-13 18:47:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls()
-  # Your code here
+def long_planeteer_calls(calls_array)
+  
 end
 
 def find_the_cheese# code an argument here
