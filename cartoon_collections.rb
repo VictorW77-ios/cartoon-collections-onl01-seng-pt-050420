8ad07b1d35c6a95764d29a590d8bad77869c2963
Victor Williams <victor.williams1030@gmail.com>
2020-05-13 19:01:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def long_planeteer_calls(calls_array)
 end
 
 def find_the_cheese(real_cheese)
-  mouse = real_cheese.detect
+  mouse = real_cheese.detect do |cheese|
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
