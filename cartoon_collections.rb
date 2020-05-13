bdc473730668e47e416a25267d4007b81c40fdd7
Victor Williams <victor.williams1030@gmail.com>
2020-05-13 18:40:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(name_call)
   dwarf = ""
   name_call.each_with_index do |name, index|
     puts dwarf << "#{index + 1}.#{name}"
-    
+  end
 end
 
 def summon_captain_planet# code an argument here
